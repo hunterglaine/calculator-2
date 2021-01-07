3be03b3455518020b3241784bdeb4ef4fc2375f2
Hunter Laine <hunterglaine@gmail.com>
2021-01-07 05:08:34 +0000

    Update pseudocode

--- calculator.py
+++ calculator.py
@@ -3,49 +3,51 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-# This is a program that runs until we press q
-    # Initialize input string variable asks enter equation 
-    # Initialize variable tokens as the tokenized version of input string
-    # Develop conditionals for each function:
-    # If input_string is 'q', break
+# Loop until told to break
+    # Create variable, input_string that asks the user to input, "Enter an equation > "
+    # Split string_input into a list at ' ' delimiter
+    # If input_string is 'q'
+    # Break from the loop
+    # Otherwise,
     # If tokens at index 0 is '+'
-    # Call the add function passing in the arguments, integers of tokens[1],[2]
+    # Call the add function passing in two arguments, floats of tokens[1], [2]
     # Else if tokens at index 0 is '-'
-    # Call the subtract function passing in the aguments same as for add function
+    # Call the subtract function passing in two arguments, floats of tokens[1], [2]
     # Otherwise if tokens at index 0 is '*'
-    # Call the multiply function passing in the same arguments as add function
+    # Call the multiply function passing in two arguments, floats of tokens[1], [2]
     # Otherwise if tokens at index 0 is '/'
-    # Call the divide function passing in the same arguments as in add
+    # Call the divide function passing in two arguments, floats of tokens[1], [2]
     # Otherwise if tokens at index 0 is 'square'
-    # Call the square function, passing in only tokens at index 1
+    # Call the square function passing in one argument, float of tokens[1]
     # Otherwise if tokens at index 0 is 'cube'
-    # Call the cube funcion, passing in only tokens at index 1
+    # Call the cube function passing in one argument, float of tokens[1]
     # Otherwise if tokens at index 0 is 'pow'
-    # Call the power function, passing in tokens at index 1 and 2
+    # Call the power function passing in two arguments, floats of tokens[1], [2]
     # Otherwise if tokens at index 0 is 'mod', 
-    # Call the mod function, passing in tokens at index 1 and 2
+    # Call the mod function passing in two arguments, floats of tokens[1], [2]
     # Otherwise print invalid equation, try again
 while True:
-    input_string = input("Enter equation:  ")
+    input_string = input("Enter an equation > ")
     tokens = input_string.split(' ')
     
     if input_string == 'q':
         break
-    if tokens[0] == '+':
-        print(add(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == '-':
-        print(subtract(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == '*':
-        print(multiply(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == '/':
-        print(divide(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == 'square':
-        print(square(float(tokens[1])))  
-    elif tokens[0] == 'cube':
-        print(cube(float(tokens[1])))
-    elif tokens[0] == 'pow':
-        print(power(float(tokens[1]),float(tokens[2]))) 
-    elif tokens[0] == 'mod':
-        print(mod(float(tokens[1]), float(tokens[2])))
     else:
-        print("Invalid input. Please enter a new equation.")
+        if tokens[0] == '+':
+            print(add(float(tokens[1]), float(tokens[2])))
+        elif tokens[0] == '-':
+            print(subtract(float(tokens[1]), float(tokens[2])))
+        elif tokens[0] == '*':
+            print(multiply(float(tokens[1]), float(tokens[2])))
+        elif tokens[0] == '/':
+            print(divide(float(tokens[1]), float(tokens[2])))
+        elif tokens[0] == 'square':
+            print(square(float(tokens[1])))  
+        elif tokens[0] == 'cube':
+            print(cube(float(tokens[1])))
+        elif tokens[0] == 'pow':
+            print(power(float(tokens[1]),float(tokens[2]))) 
+        elif tokens[0] == 'mod':
+            print(mod(float(tokens[1]), float(tokens[2])))
+        else:
+            print("Invalid input. Please enter a new equation.")
